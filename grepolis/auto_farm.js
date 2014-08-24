4bcdf8d328392262ddd1a4c67928da007526bed0
nli <nli@zynga.com>
2014-08-24 09:27:20 +0800

    add auto build

--- grepolis/auto_farm.js
+++ grepolis/auto_farm.js
@@ -78,7 +78,7 @@ function linkData()
 
 var botcheckAudio = new Audio('botcheck.mp3');
 var attackAudio = new Audio('attack.wav');
-var testAudio = new Audio('test.wav');
+var testAudio = new Audio('test.wav'); 
 
 function handleNotification(n)
 {
@@ -105,6 +105,7 @@ var blacklist0 = [10263];
 var blacklist1 = [5827];
 var blacklist_list = [blacklist0, blacklist1];
 blacklist = blacklist1;
+var autoBuild = false;
 
 function getTimeoutTime()
 {
@@ -124,6 +125,192 @@ function getRequestIntervalTime()
 {
 	return 1000 * (2 + parseInt(Math.random() * 2));
 }
+
+function doGetRecruitInfoAndRecruit()
+{
+	console.log("doGetRecruitInfoAndRecruit");
+	myAjaxGet('town_overviews', 'recruit_overview', {}, function(_data) {
+		g_data = _data.html;
+		/*
+		content = content.substr(content.indexOf("var town_data"));
+		content = content.substr(0, content.indexOf("BuildingOverview.init"));
+		eval(content);
+		startBuildLoop(building_data);
+		*/
+		});
+}
+
+function doGetBuidingInfoAndBuild()
+{
+	var autoBuild = parseInt(document.getElementById("autoBuild").value);
+	if (autoBuild == 0)
+	{
+		return;
+	}
+	console.log("doGetBuidingInfoAndBuild");
+	myAjaxGet('town_overviews', 'building_overview', {}, function(_data) {
+		var content = _data.html;
+		content = content.substr(content.indexOf("var town_data"));
+		content = content.substr(0, content.indexOf("BuildingOverview.init"));
+		eval(content);
+		startBuildLoop(building_data);
+		});
+}
+
+var buildTownIndex = 0;
+var buildTownNum = 0;
+var buildTowns = [];
+
+function buildLoopEnded()
+{
+	console.log("buildLoop ended, do nothing, you can switch to recruit process.");
+}
+
+function startBuildLoop(building_data)
+{
+	buildTownIndex = 0;
+	buildTowns = [];
+	var i = 0;
+	for (var p in building_data)
+	{
+		buildTowns[i] = p;
+		i++;
+	}
+	buildTownNum = i;
+	doBuildLoop(building_data);
+}
+
+function doBuildLoop(building_data)
+{
+	if (buildTownIndex < buildTownNum)
+	{
+		var data = building_data[buildTowns[buildTownIndex]];
+		buildTownIndex++;
+		tryBuildFromOverview(buildTowns[buildTownIndex - 1], building_data);
+	}
+	else
+	{
+		buildLoopEnded();
+	}
+}
+
+function getUserName()
+{
+	return frameWindow.ITowns.getCurrentTown().name;
+}
+
+function tryBuildFromOverview(townId, building_data)
+{
+	console.log("try build " + townId);
+	var data = building_data[townId];
+	// low
+	var buildingList = {'main' : 14,
+						'lumber' : 15,
+						'ironer' : 10, 
+						'stoner' : 8, 
+						'storage' : 15, 
+						'academy' : 16, 
+						'docks' : 10, 
+						'farm' : 20, 
+						'market' : 10, 
+						'temple' : 5, 
+						'hide' : 1, 
+						'wall' : 5, 
+						'barracks' : 10
+					};
+	if (buildOneFromList(townId, buildingList, building_data))
+	{
+		return true;
+	}
+	// medium
+	buildingList = {'main' : 24,
+						'farm' : 30, 
+						'storage' : 22, 
+						'academy' : 30, 
+						'docks' : 20, 
+						'market' : 10, 
+						'temple' : 5, 
+						'hide' : 5, 
+						'wall' : 10, 
+						'lumber' : 20,
+						'ironer' : 20, 
+						'stoner' : 20, 
+						'barracks' : 15
+					};
+	if (buildOneFromList(townId, buildingList, building_data))
+	{
+		return true;
+	}
+	// unlimited
+	// medium
+	buildingList = {'main' : 24,
+						'farm' : 99, 
+						'storage' : 99, 
+						'academy' : 99, 
+						'docks' : 20, 
+						'market' : 20, 
+						'temple' : 10, 
+						'hide' : 10, 
+						'wall' : 20, 
+						'lumber' : 99,
+						'ironer' : 99, 
+						'stoner' : 99, 
+						'barracks' : 20
+					};
+	if (buildOneFromList(townId, buildingList, building_data))
+	{
+		return true;
+	}
+}
+
+function buildOneFromList(townId, buildingList, building_data)
+{
+	var data = building_data[townId];
+	var towndata = frameWindow.ITowns.towns[townId];
+	if (towndata[0] == "n")
+	{
+		return false;
+	}
+	for (var key in buildingList)
+	{
+		var value = buildingList[key];
+		var bd = data[key];
+		if (bd.enough_resources && bd.enough_storage && !bd.has_max_level && value > bd.level)
+		{
+			var params = {
+			"town_id":townId,
+			"building_id":key,
+			"tear_down":0,
+			"no_bar":0,
+			"build_for_gold":false
+			}
+		    myAjaxPost('town_overviews', 'build_building', params, function(_data) {
+		    	console.log(key + " has been built.");
+		    	doBuildLoop(building_data);
+	    	});
+	    	return true;
+		}
+	}
+	return false;
+}
+
+function doCheckStatusAndDoSomething()
+{
+	// first get current wareh"ouse information."
+	var buildingOrders = frameWindow.ITowns.getCurrentTown().buildingOrders();
+	var orderedNum = buildingOrders.models.length;
+	var currentResources = frameWindow.ITowns.getCurrentTown().getCurrentResources();
+	var storage = frameWindow.ITowns.getCurrentTown().getStorage();
+	// decide whether the resources is too much.
+	var upperlimit = storage * 0.2;
+	if (upperlimit < currentResources.wood || upperlimit < currentResources.iron ||
+		upperlimit < currentResources.stone)
+	{
+		doGetBuidingInfoAndBuild();
+	}
+}
+
+
 
 function stopLoop()
 {
@@ -157,6 +344,7 @@ function doStartRun()
 	console.log("doStartRun");
 	townIndex = 0;
 	isGettingInfo = true;
+	lootmoodvalue = parseInt(document.getElementById("lootMoral").value);
 	var hasCaptain = parseInt(document.getElementById("hasCaptain").value);
 	if (hasCaptain != 0)
 	{
@@ -180,6 +368,7 @@ function doRun()
 function lootLoopEneded()
 {
 	// switch to other action list.	
+	doCheckStatusAndDoSomething();
 	timeoutid = setTimeout(doStartRun, getTimeoutTime());
 	console.log("successful one turn.");
 	// switch to build.
@@ -343,6 +532,11 @@ function BuildOnce()
 
 }
 
+// first to build a list
+// first recruit with half of resources.
+// second build.
+// startlist = ['resources', 'center', 'warehouse', 'academy', 'farm']
+
 // List all want to do and give priority.
 // 
 
@@ -353,7 +547,7 @@ var farminfoStage = 0;
 var farmStage = 1;
 var lootinfoStage = 2;
 var lootStage = 3;
-var townlist = [65,67,68,69,70];
+var townlist = [65,66,67,68,69,70,71,72];
 
 function doRunNoCaptain()
 {
