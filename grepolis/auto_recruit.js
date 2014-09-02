caa9c95d2653668c1083fa97df44820822a749c2
nli <nli@zynga.com>
2014-09-02 23:44:49 +0800

    fix recruit

--- grepolis/auto_recruit.js
+++ grepolis/auto_recruit.js
@@ -29,35 +29,12 @@ function doRecruitLoop(recruit_towns)
 	{
 		var data = recruit_towns[recruitTownIndex];
 		recruitTownIndex++;
-		tryRecruitPrepare(data);
+		tryRecruit(data, data.name[0]);
 	}
 	else
 	{
 		recruitLoopEnded();
 	}
-}
-
-function tryRecruitPrepare(data)
-{
-	if (data.name[0] == 'd')
-	{
-		tryRecruit(data, true);
-	}
-	else if (data.name[0] = 'a')
-	{
-		tryRecruit(data, false);
-	}
-	// other case.
-}
-
-function isDefenseUnits(name)
-{
-	return name == "sword" || name == "archer" || name == "hoplite" || name == "bireme";
-}
-
-function isAttackUnits(name)
-{
-	return name == "slinger" || name == "rider" || name == "attack_ship";
 }
 
 function getAllowedRecruitNumber(data, i)
@@ -84,52 +61,164 @@ function getAllowedRecruitNumber(data, i)
 	{
 		allowed = resources.wood / consumes.wood;
 	}
-	if (allowed >= 40 / population)
+
+	console.log("unit = " + data.units[i].id + " allowed = " + allowed);
+	if (allowed >= 50 / population)
 	{
-		return 20 / population;
+		return 30 / population;
 	}
 	return 0;
 }
 
-function tryRecruit(data, defense)
+function swapH(rl, a, b)
+{
+	var t = rl[a];
+	rl[a] = rl[b];
+	rl[b] = t;
+}
+
+function doTryRecruit(start, end, data, type)
+{
+	l = [];
+	if (data.free_population < 160)
+	{
+		return false;
+	}
+
+	resource_list = [['wood', data.resources.wood], ['stone', data.resources.stone], ['iron', data.resources.iron]];
+	if (resource_list[0][1] < resource_list[1][1])
+	{
+		swapH(resource_list, 0, 1);
+	}
+	if (resource_list[0][1] < resource_list[2][1])
+	{
+		swapH(resource_list, 0, 2);
+	}
+	if (resource_list[1][1] < resource_list[2][1])
+	{
+		swapH(resource_list, 1, 2);
+	}
+
+	var totalDef = 0;
+	var totalLight = 0;
+
+	var townInfo = frameWindow.ITowns.towns[data.id];
+
+	totalLight = ((townInfo.units().attack_ship != null) ? townInfo.units().attack_ship : 0)
+		+ ((townInfo.unitsOuter().attack_ship != null) ? townInfo.unitsOuter().attack_ship : 0)
+		+ data.orders.docks.length * 3;
+
+	// (townInfo.outer_units().sword != null) ? townInfo.outer_units().sword : 0
+
+	townInfo.unitsOuter()
+
+	totalDef = townInfo.getLandUnits().sword
+		+ ((townInfo.unitsOuter().sword != null) ? townInfo.unitsOuter().sword : 0)
+		+ townInfo.getLandUnits().hoplite
+		+ ((townInfo.unitsOuter().hoplite != null) ? townInfo.unitsOuter().hoplite : 0)
+		+ townInfo.getLandUnits().archer
+		+ ((townInfo.unitsOuter().archer != null) ? townInfo.unitsOuter().archer : 0)
+		+ data.orders.barracks.length * 30;
+
+	console.log("totalDef = " + totalDef  + ", totalLight = " + totalLight + ", type = " + type);
+
+	if (type == 'a')
+	{
+		if (resource_list[0][0] == "wood")
+		{
+			l = ['rider', 'slinger', 'hoplite'];
+		}
+		else if (resource_list[0][0] == "stone")
+		{
+			l = ['slinger', 'rider', 'hoplite'];
+		}
+		else 
+		{
+			l = ['attack_ship', 'rider', 'slinger'];
+		}
+		if (totalLight < 80)
+		{
+			l[3] = 'attack_ship';
+		}
+	}
+	else if (type == 'd')
+	{
+		if (resource_list[0][0] == "wood")
+		{
+			l = ['sword', 'hoplite', 'archer', 'bireme'];
+		}
+		else if (resource_list[0][0] == "stone")
+		{
+			l = ['bireme', 'sword', 'hoplite', 'archer'];
+		}
+		else 
+		{
+			l = ['hoplite', 'sword', 'archer', 'bireme'];
+		}
+		if (totalDef > 700)
+		{
+			l = ['bireme'];
+		}
+	}
+	else if (type == 'l')
+	{
+		l = ['attack_ship'];
+	}
+	else if (type == 'b')
+	{
+		l = ['bireme'];
+	}
+	else if (type == 'h')
+	{
+		l = ['hoplite'];
+	}
+	else if (type == 's')
+	{
+		l = ['slinger'];
+	}
+	else if (type = 'r')
+	{
+		l = ['rider'];
+	}
+	for (var id in l)
+	{
+		for (var i = start; i < end; i++)
+		{
+			if (!data.units[i].dep)
+			{
+				continue;
+			}
+			if (data.units[i].id == l[id])
+			{
+				if (tryRecruitFromOverview(data, i))
+				{
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function tryRecruit(data, type)
 {
 	// barracks
 	if (data.orders.barracks.length < 7)
 	{
-		for (var i = 0; i <= 6; i++)
+		if (doTryRecruit(0, 6, data, type))
 		{
-			if (!data.units[i].dep)
-			{
-				continue;
-			}
-			if ((defense && isDefenseUnits(data.units[i].id))
-				|| (!defense && isAttackUnits(data.units[i].id)))
-			{
-				if (tryRecruitFromOverview(data, i))
-				{
-					return;
-				}
-			}
+			return;
 		}
 	}
+
 	if (data.orders.docks.length < 7)
 	{
-		for (var i = 20; i <= 21; i++)
+		if (doTryRecruit(20, 21, data, type))
 		{
-			if (!data.units[i].dep)
-			{
-				continue;
-			}
-			if ((defense && isDefenseUnits(data.units[i].id))
-				|| (!defense && isAttackUnits(data.units[i].id)))
-			{
-				if (tryRecruitFromOverview(data, i))
-				{
-					return;
-				}
-			}
+			return;
 		}
 	}
+	doRecruitLoop(g_recruitTowns);
 }
 
 function tryRecruitFromOverview(data, i)
